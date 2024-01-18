06fda4ef97dd4707d6070c8abdb3bb02a51f1f50
TanveshVaity <tanveshvaity99@gmail.com>
2024-01-18 17:10:47 +0530

    added delete functionality

--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -12,8 +12,11 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import moment from "moment";
 import useStyles from "./styles";
+import { useDispatch} from "react-redux";
+import { deletePost } from "../../../actions/posts";
 
 const Post = ({ post, setCurrentId }) => {
+  const dispatch = useDispatch();
   const classes = useStyles();
   return (
     <Card className={classes.card}>
@@ -55,7 +58,7 @@ const Post = ({ post, setCurrentId }) => {
         <Button size="small" color="primary">
           <ThumbUpAltIcon fontSize="small" /> Like {post.likeCount}{" "}
         </Button>
-        <Button size="small" color="primary">
+        <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
           <DeleteIcon fontSize="small" /> Delete
         </Button>
       </CardActions>
--- server/src/controllers/posts.controller.js
+++ server/src/controllers/posts.controller.js
@@ -6,7 +6,7 @@ export const getPosts = async(req, res) => {
         const posts = await Post.find();
         res.status(200).json(posts);
     }catch(error){
-        res.status(404).json({message: error.message});
+        res.status(404).json({message: error});
     }
 };
 
@@ -18,7 +18,7 @@ export const createPost = async (req, res) => {
 
         res.status(201).json(newPost);
     } catch (error) {
-        res.status(409).json({ message: error.message });
+        res.status(409).json({ message: error.message});
     }
 }
 
@@ -27,11 +27,34 @@ export const updatePost = async (req, res) => {
     const post = req.body;
 
     try {
-        if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send("No post with that id");
+        if (!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(404).send("No post with that id");
+        } 
         const updatedPost = await Post.findByIdAndUpdate(id, post, { new: true });
-        if (!updatedPost) return res.status(404).json({ message: "Post not found" });
+        if (!updatedPost){
+            return res.status(404).json({ message: "Post not found" });
+        } 
         res.status(200).json(updatedPost);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(500).json({ message: error.message});
+    }
+}
+
+
+export const deletePost = async(req,res) =>{
+    const {id} = req.params;
+
+    try{
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).send("No post with that id");
+        }
+        const deletedPost= await Post.findByIdAndDelete(id);
+        if (!deletedPost) {
+            return res.status(404).json({ message: 'Post not found' });
+        }
+
+        res.status(200).json({ message: 'Post deleted successfully' });
+    }catch(error){
+        res.status(500).json({message: error.message});
     }
 }
