edd86db37c701ce39f9fe958e0dbb976c92e9c4e
TanveshVaity <tanveshvaity99@gmail.com>
2024-01-21 01:57:49 +0530

    change layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,22 @@
-import React, { Fragment, useEffect , useState} from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import memories from "./assets/memories.png";
-import useStyles from './styles';
-import Form from "./components/Form/Form";
-import Posts from "./components/Posts/Posts";
-import { useDispatch } from 'react-redux';
-import { getPosts } from './actions/posts';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./components/Home/Home";
+import Auth from './components/Auth/Auth';
 
-
-
-const App = () =>{
-  const[currentId, setCurrentId] = useState(null);
-  const classes = useStyles();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [dispatch]);
-
-  return(
-    <Fragment>
+const App = () => {
+  return (
+    <Router>
       <Container maxWidth="lg">
-        <AppBar className={classes.appBar} position="static" color="inherit">
-          <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-          <img className={classes.image} src={memories} alt="icon" height="60" />
-        </AppBar>
-        <Grow in>
-          <Container>
-            <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
-              <Grid item xs={12} sm={7}>
-                <Posts setCurrentId={setCurrentId}/>
-              </Grid>
-              <Grid item xs={12} sm={4}>
-                <Form currentId={currentId} setCurrentId={setCurrentId}/>
-              </Grid>
-            </Grid>
-          </Container>
-        </Grow>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
       </Container>
-    </Fragment>
-  )
+    </Router>
+  );
 }
 
 export default App;
